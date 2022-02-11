9ddd498feeecdcfebc2cfbe74041003f951e9f4d
matviizakrevskyi <matvii.zakrevskyi@microsoft.wsei.edu.pl>
2022-02-11 11:23:39 +0100

    Initial commit

---
+++ Controllers/ListaController.cs
@@ -0,0 +1,41 @@
+﻿using _13439_MatviiZakrevskyi_zd3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _13439_MatviiZakrevskyi_zd3.Controllers
+{
+    public class ListaController : Controller
+    {
+        public List<ZakupClass> ListaZakupow = new List<ZakupClass>();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Lista(ZakupClass zakup)
+        {
+            if (string.IsNullOrEmpty(zakup.Nazwa))
+                ModelState.AddModelError(nameof(zakup.Nazwa), "IsNullOrEmpty");
+
+            if (zakup.Liczba < 0)
+                ModelState.AddModelError(nameof(zakup.Liczba), "< 0");
+
+            if (zakup.Cena < 0)
+                ModelState.AddModelError(nameof(zakup.Cena), "< 0");
+
+            if (ModelState.IsValid)
+            {
+                ListaZakupow.Add(zakup);
+                return View(ListaZakupow);
+            }
+            else
+            {
+                return View("Index", zakup);
+            }
+            
+        }
+    }
+}
---
+++ Models/ZakupClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _13439_MatviiZakrevskyi_zd3.Models
+{
+    public class ZakupClass
+    {
+        [Required]
+        public string Nazwa { get; set; }
+        [Required]
+        [Range(0, 100000000)]
+        public int Liczba { get; set; }
+        [Required]
+        [Range(0, 100000000)]
+        public double Cena { get; set; }
+
+        public double SumaTowaru()
+        { 
+            return Cena * Liczba;
+        }
+    }
+}
